959c1a35c84b815a47ee495099312763e5ff1827
nidhinkrishna <nidhingopalank@gmail.com>
2023-06-13 11:27:58 +0530

    first commit

---
+++ webscrap.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+req = requests.get('https://stackoverflow.com/questions/tagged/python')
+soup = BeautifulSoup(req.content,'html.parser')
+res = []
+
+for i in soup.find_all("a",class_="question-hyperlink",href=True):
+        res.append(i['href'])
+for ques in res:
+        print(ques)
